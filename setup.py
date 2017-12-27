e2e641e2c7bbb11e8efec438f718196443f203b4
Jiang Bian <bianjiang@hop-dl-w01.ahc.ufl.edu>
2017-12-27 16:23:33 -0500

    add missing MANIFEST.in

--- setup.py
+++ setup.py
@@ -9,7 +9,19 @@ except ImportError:
     from distutils.core import setup
 
 __author__ = 'Jiang Bian <ji0ng.bi0n@gmail.com>'
-__version__ = '0.0.1'
+
+VERSIONFILE = "tweepy/__init__.py"
+ver_file = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, ver_file, re.M)
+
+if mo:
+    version = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+    
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+reqs = [str(req.req) for req in install_reqs]
 
 packages = [
     'tweeta'
@@ -22,13 +34,13 @@ if sys.argv[-1] == 'publish':
 setup(
     name='tweeta',
     version=__version__,
-    install_requires=['ftfy>=5.2.0', 'langid>=1.1.6'],
+    install_requires=reqs,
     author='Jiang Bian',
     author_email='ji0ng.bi0n@gmail.com',
     license=open('LICENSE').read(),
-    url='https://github.com/bianjiang/tweeta/tree/master',
+    url='https://github.com/bianjiang/tweeta',
     keywords='twitter nlp',
-    description='Various utilities for processing Twitter data (e.g., parsing, etc)',
+    description='A collection utitlity funtions to process Twittwer data (e.g., turn raw json into python object with safe checks)',
     long_description=open('README.md').read() + '\n\n' +
         open('HISTORY.md').read(),
     include_package_data=True,
@@ -38,12 +50,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        #'Topic :: Communications :: Chat',
         'Topic :: Internet',
-        #'Programming Language :: Python',
-        #'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
-        #'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ]
 )
